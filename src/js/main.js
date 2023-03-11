27a54a71b0d0a0105037cdc9073516fa999ede68
Андрей Ильин <a.ilin.v@yandex.ru>
2023-03-11 23:38:59 +0300

    upd::swiper

--- src/js/main.js
+++ src/js/main.js
@@ -33,6 +33,7 @@ const swiper = new Swiper(".swiper", {
     spaceBetween: 10,
     centeredSlides: true,
     roundLengths: true,
+    slideToClickedSlide: true,
     mousewheel: {
         invert: false,
     },
